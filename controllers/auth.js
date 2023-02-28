4d257f4b716b2f93d897e16b44e6dd06a0dd8db1
Ranjan Paul <paulranjan694@gmail.com>
2023-02-28 20:51:24 +0530

    added mongoose middleware in schema

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
+const bcrypt = require("bcryptjs")
 
 const register = async(req,res) => {
     const user = await User.create({...req.body})
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
 
 const UserSchema = new mongoose.Schema({
     name:{
@@ -10,14 +11,20 @@ const UserSchema = new mongoose.Schema({
     email:{
         type: String,
         required: [true, "Please provide email"],
-        match:[/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,"Please provide valid email"]
+        match:[/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,"Please provide valid email"],
+        unique: true
     },
     password:{
         type: String,
         required: [true, "Please provide password"],
-        minlength:3,
-        maxlength:12
+        minlength:3
     },
+})
+
+UserSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password,salt)
+    next()
 })
 
 module.exports = mongoose.model("User", UserSchema)
